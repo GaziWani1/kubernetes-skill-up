b4311e6d51a145359875d42ca126b529d118f9dc
GaziWani1 <gaziwani123@gmail.com.com>
2025-09-19 14:46:01 +0530

    deployment file

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+NAME="kubernetes-skill-up-api"
+USERNAME="gaziwani"
+IMAGE="$USERNAME/$NAME:latest"
+
+echo "Building Docker image ..."
+docker build -t $IMAGE
+
+echo "Pushing image to docker Hub ..."
+docker push $IMAGE
+
+echo "Applying Kubernetes manifests ..."
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+
+echo "Getting pods ..."
+kubectl get pods 
+
+echo "Getting service ..."
+kubectl get services
+
+echo "Fetching the main service"
+kubectl get service:$NAME
